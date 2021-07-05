de479b62cc4e20c6f67c37dc4470340336d0a298
spaced-lord <zackarynoe@gmail.com>
2021-07-05 17:52:37 -0600

    Scraped and start over

--- assets/stats.js
+++ assets/stats.js
@@ -1,50 +1,19 @@
-// const xhttp = new XMLHttpRequest();
+document
+  .getElementById("player__select")
+  .addEventListener("click", playerSelect);
 
-// document
-//   .getElementById("player__select")
-//   .addEventListener("click", playerSelect);
-
-// function playerSelect() {
-//   const subjectIdNode = document.getElementById("player__select");
-//   const value = subjectIdNode.options[subjectIdNode.selectedIndex].text;
-//   console.log(value);
-// }
-
-// function appendJSON(data) {
-//   var table = document.getElementById("stats__table");
-//   data.forEach(function (object) {
-//     var tr = document.createElement("tr");
-//     tr.innerHTML =
-//       "<td>" +
-//       object.Game +
-//       "</td>" +
-//       "<td>" +
-//       object.Attempts +
-//       "</td>" +
-//       "<td>" +
-//       object.Completions +
-//       "</td>" +
-//       "<td>" +
-//       object.Yards +
-//       "</td>" +
-//       "<td>" +
-//       object.Long +
-//       "</td>" +
-//       "<td>" +
-//       object.LongTDPass +
-//       "</td>" +
-//       "<td>" +
-//       object.Touchdowns +
-//       "</td>" +
-//       "<td>" +
-//       object.Interceptions +
-//       "</td>" +
-//       "<td>" +
-//       object.Sacks +
-//       "</td>" +
-//       table.appendChild(tr);
-//   });
-// }
-
-// xhttp.open("GET", "qb_data.json", true);
-// xhttp.send();
+function playerSelect() {
+  fetch("qb_data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let output = "<h2>Players</h2>";
+      data.forEach(function (player) {
+        output += `
+            <ul>
+                <li>Player Name: ${player}</li>
+            </ul>
+            `;
+      });
+      document.getElementById("output").innerHTML = output;
+    });
+}
